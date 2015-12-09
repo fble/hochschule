e884c1bc38a75dd4fc37a027ffcecf9f632b91cf
fble <fabian.link9c@web.de>
2015-12-09 09:25:54 -0800

    jetzt aber wirklich fast -.-

--- Scanner/src/TestScanner.cpp
+++ Scanner/src/TestScanner.cpp
@@ -7,8 +7,8 @@ int main(int argc, char **argv) {
     while((token = scanner->nextToken()) != NULL) {
         auto typ = token->getType();
         cout << "Token " << Scanner::toString(typ) << "\t"
-        << "Line: " << token->getLine()
-        << " Column: " << token->getColumn() << "\t";
+        << "Line: " << token->getLine() <<"\t"
+        << "Column: " << token->getColumn() << "\t";
         Scanner::printValue(token);
         cout << endl;
     }
